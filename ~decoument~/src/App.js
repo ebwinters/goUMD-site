df9c92c4c331cfe9946d934ca21a8d31b79a3378
ebwinters <ebwinters@comcast.net>
2018-07-01 18:39:47 -0400

    finito

--- ~decoument~/src/App.js
+++ ~decoument~/src/App.js
@@ -4,18 +4,40 @@ import logo3 from './logo3.png';
 import './App.css';
 import {Button} from './components/goUMDButton';
 import {
+  withRouter,
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
 
 const About = () => (
-  <div style={{height: '400px', backgroundColor: '#FFD200'}}>
-    <h2>About</h2>
+  <div style={{height: '400px', backgroundColor: '#FFD200', fontWeight: 'lighter'}}>
+    <h2 style={{paddingTop: '40px', fontWeight: 'lighter'}}>
+      We thought the current situation was... lacking
+    </h2>
+    <p>Any reference regarding where a certain route makes stops</p>
+    <p>The ability to advise bus route selection based on metrics such as <b>ETA</b> or <b>walking distance</b></p>
+    <p>A clean interface that people can <b>comfortably</b> navigate</p>
+    <p>A <b>no-stress</b> experience</p>
+    <p style={{paddingTop: '20px', lineHeight: '36px'}}>
+      goUMD provides a simplistic bus selection experience. All that the user has to provide <br/> is their destination, and
+      our algorithm returns the most efficient UMD bus options <br/> <b>ordered by shortest walking distance</b> necessary, along with <b>ETA</b>
+    </p>
+
+
   </div>
 )
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1
+    }
+  }
+  toggle = () => {
+    this.state.page = this.state.page + 1;
+  }
   render() {
     return (
       <Router>     
@@ -25,7 +47,7 @@ class App extends Component {
           Pick your destination. Get the fastest route. Enjoy the ride.
         </h2>
         <div style={{display: 'inline'}}>
-          <Link to="/about"><Button className="goButton">Learn More</Button></Link>
+          <Link to="/about"><Button onClick={() => this.toggle()} className="goButton">Motivation</Button></Link>
           <a href="https://itunes.apple.com/us/genre/ios/id36?mt=8" target="_blank"><Button className="goButton">App Store</Button></a>
         </div>
       <Route path="/about" component={About} />
